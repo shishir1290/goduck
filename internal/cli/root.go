c3a911d7dbd4d161d1f22c68bcaa096ed63766e2
shishir1290 <shishir1290@gmail.com>
2026-08-07 22:35:34 +0600

    feat: initialize root command and CLI structure with cobra

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -3,9 +3,19 @@ package cli
 import "github.com/spf13/cobra"
 
 var rootCmd = &cobra.Command{
-	Use:   "goduck",
-	Short: "Goduck Backend DSL",
-	Long:  "Goduck is a backend DSL that generates Go applications.",
+	Use:     "goduck",
+	Short:   "Goduck Backend DSL",
+	Long:    "Goduck is a backend DSL that generates Go applications.",
+	Version: "v0.0.1",
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Goduck",
+	Long:  `All software has versions. This is Goduck's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Printf("goduck version %s\n", rootCmd.Version)
+	},
 }
 
 func Execute() error {
@@ -18,4 +28,5 @@ func init() {
 	rootCmd.AddCommand(newCmd)
 	rootCmd.AddCommand(runCmd)
 	rootCmd.AddCommand(installCmd)
+	rootCmd.AddCommand(versionCmd)
 }
